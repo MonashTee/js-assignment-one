6159ea93deafc049ac8f16f4bb3c07a45a891566
MonashTee <akinmolayanseun@gmail.com>
2024-06-28 23:29:11 +0200

    Add correct script.js

---
+++ script.js
@@ -0,0 +1,48 @@
+// Task 1: creating a variable declaration and initialization.
+let age = 2; // Number type
+let name = "Tehillah Ojo"; // String type
+let isStudent = true;// Boolean type
+
+//logging values and types to the console.
+console.log("Age:", age, "-Type:", typeof age);
+console.log("Name:", name, "-Type:", typeof name);
+console.log("isStudent:", isStudent, "-Type:", typeof isStudent)
+
+
+
+//Task 2: Arithmetic Operations
+//creating variable a nad b and assigning values to them.
+let a = 4;
+let b = 8;
+//performing arithmetic operations and storing the results in new variables.
+let c = a + b; //Addition
+let d = b - a; //Subtraction
+let e = a * b; //Multiplication
+let f = a / b; //Division
+//logging the results of the operations to console
+console.log(c, d, e, f)
+
+
+
+//Task 3: Creating variables and assigning Comparison Operators to them.
+let x = 9
+let y = "16"
+console.log(x == y);
+console.log(x === y);
+console.log(x != y);
+console.log(x !== y);
+console.log(x > y);
+console.log(x < y);
+console.log(x >= y);
+console.log(x <= y);
+
+
+
+//Task 4: Creating variables and assigning them to Coercion 
+let numberString = "10"
+let actualNumber = 5
+//logging the Coercion to a console
+console.log(numberString + actualNumber);
+console.log(numberString - actualNumber);
+console.log(numberString * actualNumber);
+console.log(numberString / actualNumber)
