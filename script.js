c01ee3191c9ec0eac9dfcb1f89bb8dc710fcd401
MonashTee <akinmolayanseun@gmail.com>
2024-06-28 23:26:03 +0200

    Remove incorrect script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-let name = prompt("Insert your full name")
-let age = prompt("How old are you?", "")
-if(age >=20) {
-    alert("You are qualified to work on Javascript", "");
-    console.log("You are qualified to work on Javascript")
-}
-alert("You are welcome to Java Script '' (name)")
-console.log("You are welcome")
